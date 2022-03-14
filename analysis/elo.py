1061e2fb914c1ffcd6debd7f05d63c5980a6e97e
Chris Strods <chris.strods@gmail.com>
2022-03-15 08:53:32 +1100

    updated elo

--- analysis/elo.py
+++ analysis/elo.py
@@ -1,5 +1,43 @@
+import numpy as np
+import pandas as pd
+
+'''
+PARAMS
+'''
+ELO_BASE = 1500
+K_FACTOR = 40
+
+
 def get_elo(team, opponent):
     return 1 / (1 + 10 ** ((opponent - team) / 400))
+
+
+def update_score(home_pre, away_pre, result):
+
+    if result == 1:
+        home_win, away_win = 1, 0
+    elif result == 0.5:
+        home_win, away_win = 0.5, 0.5
+    else:
+        home_win, away_win = 0, 1
+
+    home_expected = (1 / (1 + 10 ** ((away_pre - home_pre) / 400)))
+    away_expected = (1 / (1 + 10 ** ((home_pre - away_pre) / 400)))
+
+    home_new = home_pre + (K_FACTOR * (home_win - home_expected))
+    away_new = away_pre + (K_FACTOR * (away_win - away_expected))
+
+    return home_new, away_new
+
+
+
+def get_winner(match):
+    if match['hscore'] > match['ascore']:
+        return 1
+    elif match['hscore'] < match['ascore']:
+        return 0
+    else:
+        return 0.5
 
 def initialise_elo(df):
 
@@ -12,4 +50,20 @@ def initialise_elo(df):
     # find first match for each team and initialise to 1500
     teams = (df['hteam'].append(df['ateam'])).unique()
     for team in teams:
-        
+        for index, row in df.iterrows():
+            if row['hteam'] == team :
+                df.at[index,'home_pre_elo'] = ELO_BASE
+                break
+            elif row['ateam'] == team:
+                df.at[index,'away_pre_elo'] = ELO_BASE
+                break
+
+
+def calculate_elo(df):
+    for index, row in df.iterrows():
+        if(row['home_pre_elo'] != 0 and
+           row['away_pre_elo'] != 0):
+
+            df.at[index, 'home_post_elo'], df.at[index, 'away_post_elo'] = \
+                update_score(row['home_pre_elo'], row['away_pre_elo'], get_winner(row))
+
--- analysis/load_data.py
+++ analysis/load_data.py
@@ -1,21 +1,27 @@
 
 from os.path import dirname, abspath
+import os, sys
 import pandas as pd
-
+sys.path.append(os.path.abspath(os.getcwd()))
+from elo import initialise_elo
 
 def load():
-    d = dirname(dirname(abspath(__file__)))
-    results = pd.read_csv(d+'/data/results.csv', low_memory=False)
-    odds = pd.read_csv(d+'/data/odds_full.csv', low_memory=False)
-    player_stats = pd.read_csv(d+'/data/playerstats.csv', low_memory=False)
-    player_details = pd.read_csv(d+'/data/playerdetails.csv', low_memory=False)
+    results = pd.read_csv('../data/results.csv', low_memory=False)
+    odds = pd.read_csv('../data/odds_full.csv', low_memory=False)
+    player_stats = pd.read_csv('../data/playerstats.csv', low_memory=False)
+    player_details = pd.read_csv('../data/playerdetails.csv', low_memory=False)
 
+    #import elo from initialise_elo
     return results, odds, player_stats, player_details
 
 
 def main():
-    results, fixtures, odds, player_details, player_stats = load()
+    results, odds, player_stats, player_details = load()
+
+    elo_results = initialise_elo(results)
 
 
 if __name__ == "__main__":
     main()
+
+#%%
